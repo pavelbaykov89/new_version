4be8cdde6915c626f4c05357bdd170627a44cf39
Demid Rakhmatullin <rdemid90@gmail.com>
2016-10-19 12:38:55 +0300

    login and registration in modals (popups)

---
+++ SLK.Web/Scripts/login.js
@@ -0,0 +1,43 @@
+﻿
+var slk = {};
+slk.login = (function() {
+
+    var _loader;
+    function setLoader(loader) {
+        _loader = loader;
+    }
+
+    function reloadPopup(event, element) {
+        event.preventDefault();
+        element = $(element);
+        var url = element.attr("href");
+        var target = $("" + element.attr("data-target"));
+        var modalContent = target.find(".modal-content");
+        //modalContent.html(_loader);
+
+        // load the url and show modal on success
+        modalContent.load(url, function () {
+            target.modal("show");
+            initMetronicLogin();
+        });
+    }
+
+    function reloadIfSuccess(data, status, xhr, formId) {
+        if (data && data.success) {
+            var modalContent = $("#" + formId).closest(".modal-content");
+            modalContent.html(_loader);
+            document.location.reload();
+        }
+    }
+
+    function initMetronicLogin() {
+        Login.init();
+    }
+
+    return {
+        setLoader: setLoader,
+        reloadPopup: reloadPopup,
+        reloadIfSuccess: reloadIfSuccess,
+        initMetronicLogin: initMetronicLogin
+    }
+})();
